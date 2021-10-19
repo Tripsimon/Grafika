c76964562200b510a4a833067a4bfcb64b35a6b5
tripsimon <tripsimon3@gmail.com>
2021-10-19 17:10:34 +0200

    Color fix Triangle

--- src/streda_12_25_c01/model/Triangle.java
+++ src/streda_12_25_c01/model/Triangle.java
@@ -17,6 +17,9 @@ public class Triangle {
     private int halfDistance;
     private int wholeDistance;
 
+    private Color rightColor = Color.LIGHT_GRAY;
+    private Color dangerColor = Color.ORANGE;
+
     public Triangle(Raster R) {
         points = new ArrayList<>();
         this.raster = R;
@@ -30,10 +33,10 @@ public class Triangle {
         System.out.println(points.size());
         if ( points.size() == 1){
 
-            lineCreator.rasterize(points.get(0).x,points.get(0).y,points.get(0).x,points.get(0).y,Color.BLUE);
+            lineCreator.rasterize(points.get(0).x,points.get(0).y,points.get(0).x,points.get(0).y,rightColor);
         }
         if ( points.size() == 2){
-            lineCreator.rasterize(points.get(0).x,points.get(0).y,points.get(1).x,points.get(1).y,Color.BLUE);
+            lineCreator.rasterize(points.get(0).x,points.get(0).y,points.get(1).x,points.get(1).y,rightColor);
 
             int midX = (points.get(1).x + points.get(0).x)/2;
             int midY = (points.get(1).y + points.get(0).y)/2;
@@ -48,10 +51,10 @@ public class Triangle {
             int newY = points.get(2).y - (halfDistance *(points.get(2).y - points.get(3).y))/wholeDistance;
             points.add(new Point(newX,newY));
 
-            lineCreator.rasterize(points.get(0).x,points.get(0).y,points.get(1).x,points.get(1).y,Color.BLUE);
-            lineCreator.rasterize(points.get(0).x,points.get(0).y,points.get(4).x,points.get(4).y,Color.BLUE);
-            lineCreator.rasterize(points.get(1).x,points.get(1).y,points.get(4).x,points.get(4).y,Color.BLUE);
-            dashedLineCreator.rasterize(points.get(2).x,points.get(2).y,points.get(3).x,points.get(3).y,Color.ORANGE);
+            lineCreator.rasterize(points.get(0).x,points.get(0).y,points.get(1).x,points.get(1).y,rightColor);
+            lineCreator.rasterize(points.get(0).x,points.get(0).y,points.get(4).x,points.get(4).y,rightColor);
+            lineCreator.rasterize(points.get(1).x,points.get(1).y,points.get(4).x,points.get(4).y,rightColor);
+            dashedLineCreator.rasterize(points.get(2).x,points.get(2).y,points.get(3).x,points.get(3).y,dangerColor);
 
         }
         if (points.size() > 5) {
